2ff2ba71cd0902c27028bc71a688c4a168a811e7
Julius Eckert <eckert.julius@gmail.com>
2025-11-28 12:45:56 +0100

    foo

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+    printWidth: 140,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+};
